082e233633b4f99c49937ab89c33d127e78f3d72
kirankumaraws2024 <kirankumaraws2024@gmail.com>
2024-12-24 22:58:15 +0530

    updated Ec2

--- terraform/V1.1.1.tf
+++ terraform/V1.1.1.tf
@@ -4,10 +4,12 @@ provider "aws" {
 
 resource "aws_instance" "web" {
 
-    ami = ""
+    ami = "ami-01816d07b1128cd2d"
     instance_type = "t2.micro"
+    key_name = "dpp"
     tags = {
       Name = "Web_server"
     }
   
 }
+
