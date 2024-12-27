bdcdc052f8d870419cf00c44a55e0718fb3f648a
kirankumaraws2024 <kirankumaraws2024@gmail.com>
2024-12-28 01:27:32 +0530

    updated egress

--- terraform/V1.1.6.tf
+++ terraform/V1.1.6.tf
@@ -56,10 +56,10 @@ resource "aws_security_group" "demo-sg" {
 
        egress {
         
-        description = "Jenkins"
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
+        
+        from_port = 0
+        to_port = 0
+        protocol = -1
         cidr_blocks = ["0.0.0.0/0"]
 
     }
